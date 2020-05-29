7772536e63bd930ca30ca8e061ac79d0fc89f420
Alix Carreon <carreonalix@gmail.com>
2020-05-29 14:49:37 -0500

    Agregando ExamPackage

---
+++ src/ExamMercadoLibre/ExamElviraRaulAlix.java
@@ -0,0 +1,37 @@
+package ExamMercadoLibre;
+
+public class ExamElviraRaulAlix {
+    public static void main(String[] args) {
+/*
+1. Abrir navegador e ingresar url: "wwww.mercadolibre.com"
+2. Ir a la barra de busqueda;
+3. Escribir producto deseado: "Control Xbox" y presionar enter;
+4. Se despliega lista de resultados relacionados con la busqueda; 
+5. Seleccionar el producto deseado;
+6. Validar que el resultado coincida con busqueda; 
+*/
+    
+    realizarLogin();
+    escribirBusqueda();
+    verResultados();
+    seleccionarProducto();
+    validarDetalleProducto();
+    
+        }
+
+    private static void validarDetalleProducto() {
+    }
+
+    private static void seleccionarProducto() {
+    }
+
+    private static void verResultados() {
+    }
+
+    private static void escribirBusqueda() {
+    }
+
+    private static void realizarLogin() {
+    }
+}
+
